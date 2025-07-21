f4a94a7db98d8ccca21f248cc2f7a0063422b57d
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 11:24:03 -0500

    style: add blue background to landing page

--- danielle-lindblom-landing-page/src/components/Card/Card.tsx
+++ danielle-lindblom-landing-page/src/components/Card/Card.tsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 import CardForm from "./CardForm";
 import CardHeader from "./CardHeader";
 import CardSuccess from "./CardSuccess";
+import CardFooter from "./CardFooter";
 
 export default function Card() {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   return (
-    <section className="rounded-[35px] mx-[15px] shadow-[0_4px_12px_0_#00000040] max-w-[768px] md:mx-auto">
+    <section className="rounded-[35px] mx-[15px] shadow-[0_4px_12px_0_#00000040] max-w-[768px] md:mx-auto bg-white z-10 relative">
       <CardHeader />
       {showSuccessMessage ? (
         <CardSuccess />
       ) : (
         <CardForm showSuccess={() => setShowSuccessMessage(true)} />
       )}
+      <CardFooter />
     </section>
   );
 }
