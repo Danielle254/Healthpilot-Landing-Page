1773b30d163a51b8cf280998237397cfbebda2d2
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 10:22:56 -0500

    feat: create the card header and make it responsive

--- danielle-lindblom-landing-page/src/components/Card/CardHeader.tsx
+++ danielle-lindblom-landing-page/src/components/Card/CardHeader.tsx
@@ -1,3 +1,22 @@
+import checkmark from "../../assets/checkmark.png";
+
 export default function CardHeader() {
-  return <div></div>;
+  return (
+    <div className="bg-header-blue text-white py-[7px] flex flex-col items-center rounded-tl-[35px] rounded-tr-[35px]">
+      <ul className="flex flex-col md:flex-row py-[9px] md:w-[664px] md:justify-between">
+        <li className="py-[8px] px-[4px] flex flex-row gap-[6px] items-center">
+          <img src={checkmark} className="h-[18px] w-[18px]" alt="checkmark" />
+          <p className="text-[18px]/[1.4] font-bold">US-based agents</p>
+        </li>
+        <li className="py-[8px] px-[4px] flex flex-row gap-[6px] items-center">
+          <img src={checkmark} className="h-[18px] w-[18px]" alt="checkmark" />
+          <p className="text-[18px]/[1.4] font-bold">No pushy salespeople</p>
+        </li>
+        <li className="py-[8px] px-[4px] flex flex-row gap-[6px] items-center">
+          <img src={checkmark} className="h-[18px] w-[18px]" alt="checkmark" />
+          <p className="text-[18px]/[1.4] font-bold">No spam calls, EVER.</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
