f87bb52f7cb6f8f501f707175c47edfe2d498861
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 09:44:45 -0500

    feat: create hero section

--- danielle-lindblom-landing-page/src/components/Hero.tsx
+++ danielle-lindblom-landing-page/src/components/Hero.tsx
@@ -1,3 +1,13 @@
 export default function Hero() {
-  return <section></section>;
+  return (
+    <section className="py-[26px] text-primary-text flex flex-col gap-[12px] items-center text-center text-balance">
+      <h1 className="font-bold text-[36px]/[1.2] tracking-[-0.36px] md:text-[48px]">
+        Let’s talk about your Medicare options.
+      </h1>
+      <p className="text-[20px]/[1.45] tracking-[-0.2px] max-w-[459px]">
+        Discuss your questions with a friendly, knowledgeable agent and choose a
+        plan with confidence.
+      </p>
+    </section>
+  );
 }
