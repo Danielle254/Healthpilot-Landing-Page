e250bbe4daf3710f917bf9f3852af4a46a15a7c7
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 08:36:48 -0500

    feat: create overall page structure with placeholder components

--- danielle-lindblom-landing-page/src/App.tsx
+++ danielle-lindblom-landing-page/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Card from "./components/Card/Card";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Card />
+    </>
+  );
 }
 
 export default App;
