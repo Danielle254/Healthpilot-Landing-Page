744c9281661c2cd2b8af86e05183bd1216840b44
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 10:22:14 -0500

    feat: create card section structure with sub-components for better organization, add state variable for showing the success message

--- danielle-lindblom-landing-page/src/components/Card/Card.tsx
+++ danielle-lindblom-landing-page/src/components/Card/Card.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import CardForm from "./CardForm";
+import CardHeader from "./CardHeader";
+import CardSuccess from "./CardSuccess";
+
 export default function Card() {
-  return <section></section>;
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  return (
+    <section className="rounded-[35px] mx-[15px] shadow-[0_4px_12px_0_#00000040] max-w-[768px] md:mx-auto">
+      <CardHeader />
+      {showSuccessMessage ? (
+        <CardSuccess />
+      ) : (
+        <CardForm showSuccess={() => setShowSuccessMessage(true)} />
+      )}
+    </section>
+  );
 }
