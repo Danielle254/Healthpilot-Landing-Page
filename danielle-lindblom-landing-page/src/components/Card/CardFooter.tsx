2134c9e0a25fc739bcaa4bb350e5da40fb433c57
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 11:24:11 -0500

    feat: add card footer

---
+++ danielle-lindblom-landing-page/src/components/Card/CardFooter.tsx
@@ -0,0 +1,9 @@
+export default function CardFooter() {
+  return (
+    <div className="rounded-bl-[35px] rounded-br-[35px] border-t-[1px] border-placeholder-gray py-[7px]">
+      <p className="text-[14px]/[1.8] tracking-[0.16px] text-header-gray text-center">
+        Support Hours: Mon - Fri: 9am-6pm CST
+      </p>
+    </div>
+  );
+}
