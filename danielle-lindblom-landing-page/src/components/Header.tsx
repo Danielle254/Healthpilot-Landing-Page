98e31d7d664d2510bdab428f59f1938b95d3e6fa
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-21 09:28:49 -0500

    feat: create header section

--- danielle-lindblom-landing-page/src/components/Header.tsx
+++ danielle-lindblom-landing-page/src/components/Header.tsx
@@ -1,3 +1,20 @@
+import logo from "../assets/logo.png";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className=" py-[16px] px-[15px] border-b-[1px] border-placeholder-gray ">
+      <div className="max-w-[1024px] flex items-center justify-between mx-auto">
+        <img src={logo} alt="EverWell logo" className="w-[175px]" />
+        <p className="italic text-header-gray text-[18px] hidden md:block">
+          Your Medicare Personal Shopper
+        </p>
+        <a
+          href="https://www.healthpilot.com"
+          className="bg-button hover:bg-button-hover py-[12px] px-[14px] rounded-[10px] font-bold text-[18px]/[1.14] text-center sm:px-[24px] sm:w-[176px] text-white"
+        >
+          Get Started
+        </a>
+      </div>
+    </header>
+  );
 }
