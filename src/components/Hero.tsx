24feef49bf36367d704d8480f2e3c3871e6f8bbe
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-07-24 09:54:26 -0500

    style: increase padding to better meet the design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
-    <section className="z-10 relative py-[26px] px-[15px] text-primary flex flex-col gap-3 items-center text-center text-balance">
-      <h1 className="font-bold text-4xl/[1.2] tracking-[-0.36px] md:text-5xl">
+    <section className="z-10 relative py-[55px] px-[15px] text-primary flex flex-col gap-3 items-center text-center text-balance">
+      <h1 className="font-bold text-4xl/[1.2] tracking-[-0.36px] md:text-5xl py-[10px]">
         Let’s talk about your Medicare options.
       </h1>
       <p className="text-xl/[1.45] tracking-[-0.2px] max-w-[459px]">
